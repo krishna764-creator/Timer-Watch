02230782b0c87f87c9880ff2cb974ad07061907b
krishna764-creator <kishan.chauhan010701@gmail.com>
2025-03-26 22:12:01 +0530

    Add files via upload

---
+++ Stop Watch Project/StopWatch.js
@@ -0,0 +1,47 @@
+
+const clock = document.querySelector(".clock");
+
+let sec = 0,
+    min = 0,
+    hr = 0,
+    IntervalId;
+
+const StartStopWatch = () => {
+
+    let second = 0;
+
+    IntervalId = setInterval(() => {
+
+        if (sec < 59)sec++;
+        
+
+        else if (min >= 59) {
+
+            hr++;
+            min = 0;
+        } 
+
+        else {
+            
+            sec = 0;
+            min++;
+           
+        }
+
+        const seconds =(sec).toString().padStart(2 , "0"),
+
+         minutes = (min).toString().padStart(2 , "0"),
+
+         hours = (hr).toString().padStart(2 , "0");
+
+       clock.innerText = (`${hours}:${minutes}:${seconds}`);
+
+    }, 1000);
+
+};
+
+const StopStartWatch = () => {
+
+    clearInterval(IntervalId);
+
+};
